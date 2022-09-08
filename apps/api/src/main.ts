791d04b31edf4489dd359e8c22736775441b89eb
Jeffrey <jeffrey@imaginecreatethrive.com>
2022-09-08 08:48:59 -0500

    turn off coors?

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,10 +9,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: console,
+    cors: false,
+  });
   const globalPrefix = 'api/';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://brokenleaf.us:${port}/${globalPrefix}`

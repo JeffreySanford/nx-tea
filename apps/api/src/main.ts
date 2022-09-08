28b1afb943e8ce3900548c777575cfd513a877a7
Jeffrey <jeffrey@imaginecreatethrive.com>
2022-09-08 08:51:22 -0500

    revert

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,10 +9,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: console,
-    cors: false,
-  });
+  const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api/';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;

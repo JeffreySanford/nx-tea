27d9e445920801263adf340e8f710a3a4b990a8f
Jeffrey <jeffrey@imaginecreatethrive.com>
2022-09-29 13:24:19 -0500

    API clean and refactor.  Adjust styling

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ProductModule } from './product/product.module';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+
+import { SubscriptionModule } from './subscription/subscription.module';
+import { SubscriptionController } from './subscription/subscription.controller';
+import { SubscriptionService } from './subscription/subscription.service';
+
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ProductModule,
+    SubscriptionModule
+  ],
+  controllers: [
+    AppController,
+    ProductController,
+    SubscriptionController
+  ],
+  providers: [
+    AppService,
+    ProductService,
+    SubscriptionService
+  ]
 })
-export class AppModule {}
+export class AppModule { }

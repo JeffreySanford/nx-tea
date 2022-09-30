be579d896492518f7363b9b6fd4b8730488fa3e5
Jeffrey <jeffrey@imaginecreatethrive.com>
2022-09-30 09:36:55 -0500

    target scss over css

--- apps/tea/src/app/logout/logout.component.ts
+++ apps/tea/src/app/logout/logout.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  styleUrls: ['./logout.component.scss']
 })
 
 export class LogoutComponent implements OnInit {

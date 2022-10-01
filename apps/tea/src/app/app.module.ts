3a1c94b9af7d0d167a053065d2dac2637964484a
Jeffrey <jeffrey@imaginecreatethrive.com>
2022-10-01 07:06:19 -0500

    styling sidebar

--- apps/tea/src/app/app.module.ts
+++ apps/tea/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LandingModule } from './landing/landing.module';
-import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LogoutComponent } from './logout/logout.component';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppRoutingModule } from './app.routing.module';
+
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './header/header.component';
+import { AppFooterComponent} from './footer/footer.component';
+
+import { LandingModule } from './landing/landing.module';
+
+
 
 @NgModule({
   imports: [
@@ -24,7 +30,13 @@ import { AppRoutingModule } from './app.routing.module';
     RouterTestingModule,
     AppRoutingModule
   ],
-  declarations: [AppComponent, LoginComponent, LogoutComponent],
+  declarations: [
+    AppHeaderComponent,
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    AppFooterComponent
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/tea/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tea-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class AppHeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

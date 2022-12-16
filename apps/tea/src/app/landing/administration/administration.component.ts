322152826f3fd0c20b536bb8e03471522bac5177
Jeffrey <jeffrey@imaginecreatethrive.com>
2022-12-16 12:25:25 -0600

    fix material table

--- apps/tea/src/app/landing/administration/administration.component.ts
+++ apps/tea/src/app/landing/administration/administration.component.ts
@@ -13,27 +13,38 @@ import { UserService } from '../../common/services/user.service';
 })
 export class AdministrationComponent implements OnInit {
   users: User[] = [];
-  @ViewChild(MatPaginator) set paginator(paginator: MatPaginator){
+  tableConstructed = false;        
+  displayedColumns = ['id', 'name', 'username'];
+  
+  @ViewChild(MatPaginator) set paginator(paginator: MatPaginator) {
     this.dataSource.paginator = paginator;
   }
-  @ViewChild(MatSort) set sort(sort: MatSort){
+  @ViewChild(MatSort) set sort(sort: MatSort) {
     this.dataSource.sort = sort;
   }
 
   dataSource = new MatTableDataSource<User>();
+
   constructor(
     private userService: UserService,
     private cd: ChangeDetectorRef) {
-    this.userService.getUsers().subscribe((users) => {
-      debugger
-      this.dataSource.data = new Array<User>(Object.values(users)[0]);
-      this.cd.detectChanges();
-    });
   }
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe(users => {
-      console.log(users);
+        Object.values(users).forEach((user: User) => {
+          this.dataSource.data.push(user);
+        });
+
+        this.tableConstructed = true;
+        this.cd.detectChanges();
     });
+  }
+
+  applyFilter(target: any) {
+    let filterValue = target.value;
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
   }
 }
